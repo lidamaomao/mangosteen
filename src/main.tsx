b15c4589cb7dd90ff0829bd80c31ad8df1d2e5c5
lidamao <916929789@qq.com>
2024-01-02 14:54:04 +0800

    创建自定义报错页面

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx';
+import { ErrorPage } from './components/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <div>Hello Wolrd!</div>,
+    errorElement: <ErrorPage />,
   },
   {
     path: '/app',
