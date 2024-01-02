cd345ea46b59522aae631310c2b5285cfbeb2471
lidamao <916929789@qq.com>
2024-01-02 23:35:32 +0800

    处理动画map内存泄漏

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,24 +1,28 @@
 import { useLocation, useOutlet } from "react-router-dom";
 import { useTransition, animated } from "@react-spring/web";
-import { ReactNode } from "react";
-
-const map: Record<string, ReactNode> = {};
+import { ReactNode, useRef } from "react";
 
 const WelcomeLayout = () => {
   const location = useLocation();
   const outlet = useOutlet();
-  map[location.pathname] = outlet;
+  const map = useRef<Record<string, ReactNode>>({});
+  map.current[location.pathname] = outlet;
   const transitions = useTransition(location.pathname, {
-    from: { transform: "translateX(100%)" },
+    from: {
+      transform:
+        location.pathname === "/welcome/1"
+          ? "translateX(0%)"
+          : "translateX(100%)",
+    },
     enter: { transform: "translateX(0%)" },
     leave: { transform: "translateX(-100%)" },
-    config: { duration: 1000 },
+    config: { duration: 300 },
   });
 
   return transitions((style, pathname) => {
     return (
       <animated.div key={pathname} style={style}>
-        <div style={{ textAlign: "center" }}>{map[pathname]}</div>
+        <div style={{ textAlign: "center" }}>{map.current[pathname]}</div>
       </animated.div>
     );
   });
