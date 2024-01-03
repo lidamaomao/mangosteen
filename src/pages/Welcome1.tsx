c954d9fb627e541081cdf500359150d03d88feea
lidamao <916929789@qq.com>
2024-01-04 00:38:19 +0800

    完成第一个HTML

--- src/pages/Welcome1.tsx
+++ src/pages/Welcome1.tsx
@@ -1,5 +1,16 @@
+import welcome1Icon from "../assets/images/welcome1.svg";
+
 const Welcome1 = () => {
-  return <div>1</div>;
+  return (
+    <div>
+      <img src={welcome1Icon} alt="pig icon" />
+      <p>
+        会挣钱
+        <br />
+        还要会省钱
+      </p>
+    </div>
+  );
 };
 
 export default Welcome1;
